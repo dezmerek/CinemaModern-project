c4ec9a5fea5c33d3f2916d727b7138e914aecf30
dezmerek <mszymanski027@gmail.com>
2023-10-17 11:32:30 +0200

    Added user data, create /data

--- frontend/src/components/dashboard/UsersView/UsersView.jsx
+++ frontend/src/components/dashboard/UsersView/UsersView.jsx
@@ -1,38 +1,8 @@
 import React from 'react';
 import GenericTable from '../UniversalTable/UniversalTable';
+import usersData from '../../../data/usersData';
 
 const UsersView = () => {
-  const usersData = [
-    {
-      id: 1,
-      firstName: 'Konrad',
-      lastName: 'Górski',
-      role: 'Użytkownik',
-      phoneNumber: '349566253',
-    },
-    {
-      id: 2,
-      firstName: 'Patryk',
-      lastName: 'Baranowski',
-      role: 'Pracownik',
-      phoneNumber: '253349566',
-    },
-    {
-      id: 3,
-      firstName: 'Oliwier',
-      lastName: 'Wojciechowski',
-      role: 'Analityk',
-      phoneNumber: '696345253',
-    },
-    {
-      id: 4,
-      firstName: 'Adrian',
-      lastName: 'Kwiatkowski',
-      role: 'Administrator',
-      phoneNumber: '566234953',
-    },
-  ];
-
   const userColumns = ['id', 'firstName', 'lastName', 'role', 'phoneNumber'];
   const translatedColumns = [
     'ID',
