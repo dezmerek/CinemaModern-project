46928ac2cae72e71b86717e8669540c7b540e221
dezmerek <mszymanski027@gmail.com>
2023-10-22 02:59:42 +0200

    Add Sidebar to UsersView

--- frontend/src/routers/Users.js
+++ frontend/src/routers/Users.js
@@ -1,10 +1,14 @@
 import React from "react";
 import UsersView from '../components/dashboard/UsersView/UsersView';
+import Sidebar from '../components/dashboard/Sidebar/Sidebar';
 
 export const Users = () => {
     return (
-        <>
-            <UsersView />
-        </>
+        <div style={{ display: 'flex', gap: '3rem' }}>
+            <Sidebar />
+            <div style={{ flexBasis: '100%' }}>
+                <UsersView />
+            </div>
+        </div>
     );
 };
