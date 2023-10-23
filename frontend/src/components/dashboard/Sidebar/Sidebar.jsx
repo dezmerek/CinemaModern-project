e3d9d39a816c484ff30c493ea8acbc97badc15e4
dezmerek <mszymanski027@gmail.com>
2023-10-23 19:50:15 +0200

    Sidebar and responsiveness update

--- frontend/src/components/dashboard/Sidebar/Sidebar.jsx
+++ frontend/src/components/dashboard/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import menuItems from './SidebarItems';
 import '../../../Styles/components/_Sidebar.scss';
@@ -7,31 +7,52 @@ import avatar from '../../../assets/images/avatar.png';
 
 const Sidebar = () => {
   const location = useLocation();
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const [isHamburgerActive, setHamburgerActive] = useState(false);
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+    setHamburgerActive(!isHamburgerActive);
+  };
 
   return (
-    <div className="sidebar">
-      <div className="sidebar__logo">
-        <img src={logo} alt="logo" />
-      </div>
-      <div className="profile">
-        <img src={avatar} alt="avatar" />
-        <div className="profile__info">
-          <p className="profile__info-role">Admin</p>
-          <p>Jan Kowalski</p>
+    <>
+      <div className="navbar">
+        <div className="navbar__logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div
+          className={`navbar__toggle ${isHamburgerActive ? 'is-active' : ''}`}
+          onClick={toggleSidebar}
+        >
+          <div className="hamburger">
+            <span></span>
+          </div>
         </div>
       </div>
-      <div className="menu">
-        {menuItems.map((item, index) => (
-          <Link
-            to={item.link}
-            key={index}
-            className={location.pathname === item.link ? 'active' : ''}
-          >
-            {item.icon} {item.name}
-          </Link>
-        ))}
+      <div className={`sidebar ${isSidebarOpen ? 'is-active' : ''}`}>
+        <div className="sidebar__logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="profile">
+          <img src={avatar} alt="avatar" />
+          <div className="profile__info">
+            <p className="profile__info-role">Admin</p>
+            <p>Jan Kowalski</p>
+          </div>
+        </div>
+        <div className="menu">
+          {menuItems.map((item, index) => (
+            <Link
+              to={item.link}
+              key={index}
+              className={location.pathname === item.link ? 'active' : ''}
+            >
+              {item.icon} {item.name}
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- frontend/src/routers/Users.js
+++ frontend/src/routers/Users.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UsersView from '../components/dashboard/UsersView/UsersView';
 import Sidebar from '../components/dashboard/Sidebar/Sidebar';
 
 export const Users = () => {
+    const [isFlex, setIsFlex] = useState(window.innerWidth >= 992);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsFlex(window.innerWidth >= 992);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <>
-            <Sidebar />
-            <UsersView />
+            <div style={isFlex ? { display: 'flex', gap: '3rem' } : {}}>
+                <Sidebar />
+                <div style={isFlex ? { flex: 1 } : {}}>
+                    <UsersView />
+                </div>
+            </div>
         </>
     );
 };
