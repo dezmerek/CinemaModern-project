3e8013c05f1b405a12762706918d2c119645e5f5
dezmerek <mszymanski027@gmail.com>
2023-10-28 00:56:47 +0200

    Responsive layout for Films dashboard with sidebar

--- frontend/src/routers/Films.js
+++ frontend/src/routers/Films.js
@@ -1,12 +1,30 @@
-import React from "react";
-import Sidebar from "../components/dashboard/Sidebar/Sidebar";
-import FilmView from "../components/dashboard/FilmView/FilmView";
+import React, { useEffect, useState } from "react";
+import FilmView from '../components/dashboard/FilmView/FilmView';
+import Sidebar from '../components/dashboard/Sidebar/Sidebar';
 
 export const Films = () => {
+    const [isFlex, setIsFlex] = useState(window.innerWidth >= 992);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsFlex(window.innerWidth >= 992);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <>
-            <Sidebar />
-            <FilmView />
+            <div style={isFlex ? { display: 'flex', gap: '3rem' } : {}}>
+                <Sidebar />
+                <div style={isFlex ? { flex: 1 } : {}}>
+                    <FilmView />
+                </div>
+            </div>
         </>
     );
 };
