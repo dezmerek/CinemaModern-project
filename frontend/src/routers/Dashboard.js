d8322e50396850b90094c8896bb58aa39f788628
Dezmerek <mszymanski027@gmail.com>
2023-11-14 14:02:55 +0100

    Add DashboardView component

--- frontend/src/routers/Dashboard.js
+++ frontend/src/routers/Dashboard.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "../components/dashboard/Sidebar/Sidebar";
+import DashboardView from "../components/dashboard/Dashboard/DashboardView";
 
 export const Dashboard = () => {
+    const [isFlex, setIsFlex] = useState(window.innerWidth >= 992);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsFlex(window.innerWidth >= 992);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
-        <>
+        <> <div style={isFlex ? { display: 'flex', gap: '3rem' } : {}}>
             <Sidebar />
+            <div style={isFlex ? { flex: 1 } : {}}>
+                <DashboardView />
+            </div></div>
         </>
     );
 };
