c845738d11a19ecc5d1216f8283025ebbd455975
dezmerek <mszymanski027@gmail.com>
2023-11-16 03:15:26 +0100

    Add dashboard statistics section - 30 last day

--- frontend/src/components/dashboard/Dashboard/DashboardView.jsx
+++ frontend/src/components/dashboard/Dashboard/DashboardView.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import '../../../Styles/layout/_Dashboard.scss';
 
 const DashboardView = () => {
   return (
     <>
-      <div>Dashboard</div>
+      <h2>Dashboard</h2>
+
+      <div className="dashboard">
+        <div className="dashboard__stats">
+          <div className="dashboard__stat-box">
+            <p>Sprzedanych biletów</p>
+            <p className="dashboard__stat-box--30-last">Ostatnie 30 dni</p>
+            <h1>2137</h1>
+          </div>
+          <div className="dashboard__stat-box">
+            <p>Nowych filmów</p>
+            <p className="dashboard__stat-box--30-last">Ostatnie 30 dni</p>
+            <h1>2137</h1>
+          </div>
+          <div className="dashboard__stat-box">
+            <p>Nowych użytkowników</p>
+            <p className="dashboard__stat-box--30-last">Ostatnie 30 dni</p>
+            <h1>2137</h1>
+          </div>
+          <div className="dashboard__stat-box">
+            <p>Napisanych Rezenzji</p>
+            <p className="dashboard__stat-box--30-last">Ostatnie 30 dni</p>
+            <h1>2137</h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
