254044dcae42361627959c49751311491566e8d1
dezmerek <mszymanski027@gmail.com>
2023-11-16 16:47:48 +0100

    Add Dashboard card

--- frontend/src/components/dashboard/Dashboard/DashboardView.jsx
+++ frontend/src/components/dashboard/Dashboard/DashboardView.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import DashboardStat from './DashboardStat';
 import '../../../Styles/layout/_Dashboard.scss';
 
+import { BsArrowUpRightSquare, BsStar } from 'react-icons/bs';
+
 const DashboardView = () => {
   return (
     <>
@@ -29,6 +31,269 @@ const DashboardView = () => {
             subtitle="Ostatnie 30 dni"
             value={2137}
           />
+        </div>
+
+        <div className="dashboard__cards">
+          <div className="dashboard__card">
+            <div>
+              <h4>
+                <BsArrowUpRightSquare />
+                Ostatnia recenzja
+              </h4>
+              <button>Wszystkie</button>
+            </div>
+            <hr />
+            <table>
+              <thead>
+                <th>ID</th>
+                <th>TYTUŁ</th>
+                <th>JĘZYK</th>
+                <th>OCENA</th>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>221</td>
+                  <td>Aftersun</td>
+                  <td>Polski</td>
+                  <td>
+                    <BsStar />
+                    8.4
+                  </td>
+                </tr>
+                <tr>
+                  <td>221</td>
+                  <td>Aftersun</td>
+                  <td>Polski</td>
+                  <td>
+                    <BsStar />
+                    8.4
+                  </td>
+                </tr>
+                <tr>
+                  <td>221</td>
+                  <td>Aftersun</td>
+                  <td>Polski</td>
+                  <td>
+                    <BsStar />
+                    8.4
+                  </td>
+                </tr>
+                <tr>
+                  <td>221</td>
+                  <td>Aftersun</td>
+                  <td>Polski</td>
+                  <td>
+                    <BsStar />
+                    8.4
+                  </td>
+                </tr>
+                <tr>
+                  <td>221</td>
+                  <td>Aftersun</td>
+                  <td>Polski</td>
+                  <td>
+                    <BsStar />
+                    8.4
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="dashboard__card">
+            <div>
+              <h4>
+                <BsArrowUpRightSquare />
+                Ostatnia recenzja
+              </h4>
+              <button>Wszystkie</button>
+            </div>
+            <hr />
+            <table>
+              <thead>
+                <th>ID</th>
+                <th>TYTUŁ</th>
+                <th>JĘZYK</th>
+                <th>OCENA</th>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>221</td>
+                  <td>Aftersun</td>
+                  <td>Polski</td>
+                  <td>
+                    <BsStar />
+                    8.4
+                  </td>
+                </tr>
+                <tr>
+                  <td>221</td>
+                  <td>Aftersun</td>
+                  <td>Polski</td>
+                  <td>
+                    <BsStar />
+                    8.4
+                  </td>
+                </tr>
+                <tr>
+                  <td>221</td>
+                  <td>Aftersun</td>
+                  <td>Polski</td>
+                  <td>
+                    <BsStar />
+                    8.4
+                  </td>
+                </tr>
+                <tr>
+                  <td>221</td>
+                  <td>Aftersun</td>
+                  <td>Polski</td>
+                  <td>
+                    <BsStar />
+                    8.4
+                  </td>
+                </tr>
+                <tr>
+                  <td>221</td>
+                  <td>Aftersun</td>
+                  <td>Polski</td>
+                  <td>
+                    <BsStar />
+                    8.4
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="dashboard__card">
+            <div>
+              <h4>
+                <BsArrowUpRightSquare />
+                Ostatnia recenzja
+              </h4>
+              <button>Wszystkie</button>
+            </div>
+            <hr />
+            <table>
+              <thead>
+                <th>ID</th>
+                <th>TYTUŁ</th>
+                <th>JĘZYK</th>
+                <th>OCENA</th>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>221</td>
+                  <td>Aftersun</td>
+                  <td>Polski</td>
+                  <td>
+                    <BsStar />
+                    8.4
+                  </td>
+                </tr>
+                <tr>
+                  <td>221</td>
+                  <td>Aftersun</td>
+                  <td>Polski</td>
+                  <td>
+                    <BsStar />
+                    8.4
+                  </td>
+                </tr>
+                <tr>
+                  <td>221</td>
+                  <td>Aftersun</td>
+                  <td>Polski</td>
+                  <td>
+                    <BsStar />
+                    8.4
+                  </td>
+                </tr>
+                <tr>
+                  <td>221</td>
+                  <td>Aftersun</td>
+                  <td>Polski</td>
+                  <td>
+                    <BsStar />
+                    8.4
+                  </td>
+                </tr>
+                <tr>
+                  <td>221</td>
+                  <td>Aftersun</td>
+                  <td>Polski</td>
+                  <td>
+                    <BsStar />
+                    8.4
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="dashboard__card">
+            <div>
+              <h4>
+                <BsArrowUpRightSquare />
+                Ostatnia recenzja
+              </h4>
+              <button>Wszystkie</button>
+            </div>
+            <hr />
+            <table>
+              <thead>
+                <th>ID</th>
+                <th>TYTUŁ</th>
+                <th>JĘZYK</th>
+                <th>OCENA</th>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>221</td>
+                  <td>Aftersun</td>
+                  <td>Polski</td>
+                  <td>
+                    <BsStar />
+                    8.4
+                  </td>
+                </tr>
+                <tr>
+                  <td>221</td>
+                  <td>Aftersun</td>
+                  <td>Polski</td>
+                  <td>
+                    <BsStar />
+                    8.4
+                  </td>
+                </tr>
+                <tr>
+                  <td>221</td>
+                  <td>Aftersun</td>
+                  <td>Polski</td>
+                  <td>
+                    <BsStar />
+                    8.4
+                  </td>
+                </tr>
+                <tr>
+                  <td>221</td>
+                  <td>Aftersun</td>
+                  <td>Polski</td>
+                  <td>
+                    <BsStar />
+                    8.4
+                  </td>
+                </tr>
+                <tr>
+                  <td>221</td>
+                  <td>Aftersun</td>
+                  <td>Polski</td>
+                  <td>
+                    <BsStar />
+                    8.4
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </>
