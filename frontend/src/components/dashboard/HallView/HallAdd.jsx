1cf9bee9a4dca136173c1498e75c12cd002daf65
dezmerek <mszymanski027@gmail.com>
2023-11-18 14:37:53 +0100

    Creating the structure and styles HallAdd form

--- frontend/src/components/dashboard/HallView/HallAdd.jsx
+++ frontend/src/components/dashboard/HallView/HallAdd.jsx
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../../../Styles/layout/_HallAdd.scss';
 
 const HallAdd = () => {
+  const [selectedBanner, setSelectedBanner] = useState(null);
+
+  const handleBannerChange = (event) => {
+    const selectedFile = event.target.files[0];
+    if (selectedFile) {
+      const reader = new FileReader();
+
+      const image = new Image();
+      image.src = URL.createObjectURL(selectedFile);
+      image.onload = () => {
+        if (image.width === 445 && image.height === 333) {
+          reader.onload = (event) => {
+            setSelectedBanner(event.target.result);
+          };
+          reader.readAsDataURL(selectedFile);
+        } else {
+          alert('Obraz baneru musi mieć wymiary 445x333 pikseli.');
+          event.target.value = '';
+        }
+      };
+    }
+  };
+
   return (
     <div>
-      <h2>Dodaj sale</h2>
+      <h2>Dodaj salę kinową</h2>
+
+      <form id="hallAddForm" className="hall-add">
+        <div className="hall-add__container">
+          <div className="hall-add__banner">
+            <input
+              type="file"
+              name="bannerImage"
+              accept="image/*"
+              onChange={handleBannerChange}
+              required
+            />
+            {selectedBanner && (
+              <img src={selectedBanner} alt="Podgląd obrazu" />
+            )}
+            {!selectedBanner && (
+              <label>
+                Dodaj baner<br></br>(445 x 333)
+              </label>
+            )}
+          </div>
+
+          <div className="hall-add__content">
+            <input type="text" placeholder="Nazwa sali" required />
+            <textarea
+              type="text"
+              name="description"
+              placeholder="Opis"
+              required
+            ></textarea>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
