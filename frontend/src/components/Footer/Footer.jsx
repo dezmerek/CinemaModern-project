ebdfe5936590e84df027e6546eb1053df009be1a
Dezmerek <mszymanski027@gmail.com>
2023-11-21 13:53:30 +0100

    Add content and styling to Footer component

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
+import '../../Styles/components/_Footer.scss';
+import { BsArrowUp } from 'react-icons/bs';
 
 const Footer = () => {
-  return <div>todo Footer</div>;
+  return (
+    <>
+      <div className="footer">
+        <img src={logo} alt="logo" />
+        <div>
+          <Link to="/regulamin">Regulamin</Link>
+          <Link to="/polityka-prywatnosci">Polityka Prywatności</Link>
+          <button>
+            <BsArrowUp />
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
