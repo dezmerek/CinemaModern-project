d863136bf41bfc7d3d1eaabca35ce0f5e92560db
Dezmerek <mszymanski027@gmail.com>
2023-11-21 03:53:41 +0100

    Create basic component Footer

--- frontend/src/routers/Home.js
+++ frontend/src/routers/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Banner from "../components/Banner/Banner";
 import RecommendedVideos from "../components/RecommendedVideos/RecommendedVideos";
+import Footer from "../components/Footer/Footer";
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Navbar />
             <Banner />
             <RecommendedVideos />
+            <Footer />
         </div>
     );
 }
