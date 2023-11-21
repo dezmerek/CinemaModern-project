d251110ed658fb24bcd475bfba6b0f6752d6aaa0
Dezmerek <mszymanski027@gmail.com>
2023-11-21 03:46:43 +0100

    Create basic component Banner

--- frontend/src/routers/Home.js
+++ frontend/src/routers/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
+import Banner from "../components/Banner/Banner";
+
 
 const Home = () => {
     return (
         <div>
             <Navbar />
+            <Banner />
         </div>
     );
 }
