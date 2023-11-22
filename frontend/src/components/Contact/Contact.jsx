b9c4d1e4c3bf3ababd16fc76afa9ca3009991819
dezmerek <mszymanski027@gmail.com>
2023-11-22 13:32:56 +0100

    Adding contact info and social content

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import { FaFacebook, FaTwitter, FaInstagram, FaYoutube } from 'react-icons/fa';
+import '../../Styles/components/_Contact.scss';
 
 const Contact = () => {
-  return <div>todo Contact</div>;
+  return (
+    <div className="contact">
+      <div className="contact__container">
+        <h2>Kontakt</h2>
+        <div className="contact__info">
+          <div>
+            <h3>Adres</h3>
+            31-150 Kraków, Świętego Filipa 17
+          </div>
+          <div>
+            <h3>Telefon</h3>
+            +48 555 444 888
+          </div>
+          <div>
+            <h3>Email</h3>
+            mail@cinema.com
+          </div>
+
+          <div className="contact__social">
+            <FaFacebook />
+            <FaTwitter />
+            <FaInstagram />
+            <FaYoutube />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
