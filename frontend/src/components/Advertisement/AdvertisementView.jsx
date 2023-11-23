15fe07cec3573a4cd938e7be587b2be97d513daf
dezmerek <mszymanski027@gmail.com>
2023-11-23 14:10:52 +0100

    Add content and styles to ad component

--- frontend/src/components/Advertisement/AdvertisementView.jsx
+++ frontend/src/components/Advertisement/AdvertisementView.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import '../../Styles/components/_AdvertisementView.scss';
+import '../../Styles/components/_Advertisement.scss';
+import AdvertisementImage from '../../assets/images/advertisement-image.png';
 
 const AdvertisementView = () => {
   return (
-    <>
-      <h2>test</h2>
-    </>
+    <div className="advertisement">
+      <div className="advertisement__container">
+        <div>
+          <h1>OH!</h1>
+          <h3>Zyskaj więcej dzięki karcie Cinema Club</h3>
+          <h2>Dołącz do naszego programu lojalnościowego</h2>
+
+          <Link to="/">
+            <button type="button">Czytaj więcej</button>
+          </Link>
+        </div>
+        <img src={AdvertisementImage} alt="advertisement" />
+      </div>
+    </div>
   );
 };
 
