2adf02f9fc71328f7f6a2aca615e6a7734ec5e37
dezmerek <mszymanski027@gmail.com>
2023-11-28 04:13:45 +0100

    Fix movieDetails in MoviePreviewsDetail

--- frontend/src/components/home/MoviePreviews/MoviePreviewsDetail.jsx
+++ frontend/src/components/home/MoviePreviews/MoviePreviewsDetail.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { format } from 'date-fns';
-import '../../../Styles/layout/_MoviePreviewsDetail.scss';
 import { pl } from 'date-fns/locale';
+import '../../../Styles/layout/_MoviePreviewsDetail.scss';
 
 const MoviePreviewsDetail = () => {
   const { id } = useParams();
@@ -30,51 +30,53 @@ const MoviePreviewsDetail = () => {
   return (
     <div className="movie-previews-detail">
       <div className="movie-previews-detail__container">
-        <h1>{movieDetails.title}</h1>
-        <p>{movieDetails.duration} min</p>
-        {format(new Date(movieDetails.releaseDateWorld), 'dd MMMM yyyy', {
-          locale: pl,
-        })}
-
-        <p>{movieDetails.description}</p>
-
-        <>
-          <img
-            src={`${process.env.REACT_APP_API_URL}/images/trailerBanners/${movieDetails.trailerBannerImage}`}
-            alt="Trailer Banner"
-          />
-          <img
-            src={`${process.env.REACT_APP_API_URL}/images/movieBanners/${movieDetails.mainBannerImage}`}
-            alt="Main Banner"
-          />
-        </>
-
         {movieDetails && (
-          <table>
-            <tbody>
-              <tr>
-                <th>reżyseria</th>
-                <td>{movieDetails.director}</td>
-              </tr>
-              <tr>
-                <th>scenariusz</th>
-                <td>{movieDetails.writer}</td>
-              </tr>
-              <tr>
-                <th>premiera</th>
-                <td>
-                  {new Date(movieDetails.releaseDateWorld).toLocaleDateString()}
-                  (Światowa premiera)
-                </td>
-                <td>
-                  {new Date(
-                    movieDetails.releaseDatePoland
-                  ).toLocaleDateString()}
-                  (Polska premiera kinowa)
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <>
+            <h1>{movieDetails.title}</h1>
+
+            <img
+              src={`${process.env.REACT_APP_API_URL}/images/movieBanners/${movieDetails.mainBannerImage}`}
+              alt="Main Banner"
+            />
+            {format(new Date(movieDetails.releaseDatePoland), 'dd MMMM yyyy', {
+              locale: pl,
+            })}
+            <p>{movieDetails.duration} min</p>
+            <p>{movieDetails.description}</p>
+
+            <table>
+              <tbody>
+                <tr>
+                  <th>reżyseria</th>
+                  <td>{movieDetails.director}</td>
+                </tr>
+                <tr>
+                  <th>scenariusz</th>
+                  <td>{movieDetails.writer}</td>
+                </tr>
+                <tr>
+                  <th>premiera</th>
+                  <td>
+                    {new Date(
+                      movieDetails.releaseDateWorld
+                    ).toLocaleDateString()}{' '}
+                    (Światowa premiera)
+                  </td>
+                  <td>
+                    {new Date(
+                      movieDetails.releaseDatePoland
+                    ).toLocaleDateString()}{' '}
+                    (Polska premiera kinowa)
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+
+            <img
+              src={`${process.env.REACT_APP_API_URL}/images/trailerBanners/${movieDetails.trailerBannerImage}`}
+              alt="Trailer Banner"
+            />
+          </>
         )}
       </div>
     </div>
