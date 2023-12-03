b0b540ad82681b275546eeb3a3f8ac7539c8d63b
dezmerek <mszymanski027@gmail.com>
2023-12-03 21:46:20 +0100

    Update styles preview detail

--- frontend/src/components/home/MoviePreviews/MoviePreviewsDetail.jsx
+++ frontend/src/components/home/MoviePreviews/MoviePreviewsDetail.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import { format } from 'date-fns';
 import { pl } from 'date-fns/locale';
 import '../../../Styles/layout/_MoviePreviewsDetail.scss';
+import { BsPlayCircle } from 'react-icons/bs';
 
 const MoviePreviewsDetail = () => {
   const { id } = useParams();
@@ -34,48 +35,117 @@ const MoviePreviewsDetail = () => {
           <>
             <h1>{movieDetails.title}</h1>
 
-            <img
-              src={`${process.env.REACT_APP_API_URL}/images/movieBanners/${movieDetails.mainBannerImage}`}
-              alt="Main Banner"
-            />
-            {format(new Date(movieDetails.releaseDatePoland), 'dd MMMM yyyy', {
-              locale: pl,
-            })}
-            <p>{movieDetails.duration} min</p>
-            <p>{movieDetails.description}</p>
-
-            <table>
-              <tbody>
-                <tr>
-                  <th>reżyseria</th>
-                  <td>{movieDetails.director}</td>
-                </tr>
-                <tr>
-                  <th>scenariusz</th>
-                  <td>{movieDetails.writer}</td>
-                </tr>
-                <tr>
-                  <th>premiera</th>
-                  <td>
-                    {new Date(
-                      movieDetails.releaseDateWorld
-                    ).toLocaleDateString()}{' '}
-                    (Światowa premiera)
-                  </td>
-                  <td>
-                    {new Date(
-                      movieDetails.releaseDatePoland
-                    ).toLocaleDateString()}{' '}
-                    (Polska premiera kinowa)
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-
-            <img
-              src={`${process.env.REACT_APP_API_URL}/images/trailerBanners/${movieDetails.trailerBannerImage}`}
-              alt="Trailer Banner"
-            />
+            <div className="movie-previews-detail__info-first">
+              <img
+                src={`${process.env.REACT_APP_API_URL}/images/movieBanners/${movieDetails.mainBannerImage}`}
+                alt="Main Banner"
+              />
+              <div>
+                <div className="movie-previews-detail__info-short">
+                  <h3>
+                    {format(
+                      new Date(movieDetails.releaseDatePoland),
+                      'dd MMMM yyyy',
+                      {
+                        locale: pl,
+                      }
+                    )}
+                  </h3>
+                  <h3>{movieDetails.duration} min</h3>
+                </div>
+                <p>{movieDetails.description}</p>
+                <div className="movie-previews-detail__long-info1">
+                  <div className="movie-previews-detail__table">
+                    <table>
+                      <tbody>
+                        <tr>
+                          <th>reżyseria</th>
+                          <td>{movieDetails.director}</td>
+                        </tr>
+                        <tr>
+                          <th>scenariusz</th>
+                          <td>{movieDetails.writer}</td>
+                        </tr>
+                        <tr>
+                          <th>gatunek</th>
+                          <td>{movieDetails.genres.join(', ')}</td>
+                        </tr>
+                        <tr>
+                          <th>premiera</th>
+                          <td>
+                            {new Date(
+                              movieDetails.releaseDateWorld
+                            ).toLocaleDateString()}{' '}
+                            (Światowa premiera)
+                          </td>
+                        </tr>
+                        <tr>
+                          <th></th>
+                          <td>
+                            {new Date(
+                              movieDetails.releaseDatePoland
+                            ).toLocaleDateString()}{' '}
+                            (Polska premiera kinowa)
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <div className="movie-previews-detail__trailer">
+                      <img
+                        src={`${process.env.REACT_APP_API_URL}/images/trailerBanners/${movieDetails.trailerBannerImage}`}
+                        alt="Trailer Banner"
+                      />
+                      <BsPlayCircle />
+                    </div>
+                  </div>
+                </div>{' '}
+              </div>
+            </div>
+            <div className="movie-previews-detail__long-info2">
+              <div className="movie-previews-detail__table">
+                <table>
+                  <tbody>
+                    <tr>
+                      <th>reżyseria</th>
+                      <td>{movieDetails.director}</td>
+                    </tr>
+                    <tr>
+                      <th>scenariusz</th>
+                      <td>{movieDetails.writer}</td>
+                    </tr>
+                    <tr>
+                      <th>gatunek</th>
+                      <td>{movieDetails.genres.join(', ')}</td>
+                    </tr>
+                    <tr>
+                      <th>premiera</th>
+                      <td>
+                        {new Date(
+                          movieDetails.releaseDateWorld
+                        ).toLocaleDateString()}{' '}
+                        (Światowa premiera)
+                      </td>
+                    </tr>
+                    <tr>
+                      <th></th>
+                      <td>
+                        {new Date(
+                          movieDetails.releaseDatePoland
+                        ).toLocaleDateString()}{' '}
+                        (Polska premiera kinowa)
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+                <div className="movie-previews-detail__trailer">
+                  <img
+                    src={`${process.env.REACT_APP_API_URL}/images/trailerBanners/${movieDetails.trailerBannerImage}`}
+                    alt="Trailer Banner"
+                  />
+                  <BsPlayCircle />
+                </div>
+              </div>
+            </div>
           </>
         )}
       </div>
