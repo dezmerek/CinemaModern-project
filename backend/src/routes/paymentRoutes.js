04c140e2fa0c6ac33e6d17685dac85d8495a375f
dezmerek <mszymanski027@gmail.com>
2023-12-14 15:41:23 +0100

    Edit url notification tpay env

--- backend/src/routes/paymentRoutes.js
+++ backend/src/routes/paymentRoutes.js
@@ -28,7 +28,7 @@ router.post('/create-session-transaction-tpay', async (req, res) => {
                     error: 'http://localhost:3000/',
                 },
                 notification: {
-                    url: 'https://f4b9-2a02-a31a-a241-1300-794a-1d68-c666-9906.ngrok-free.app/api/payments/tpay-notifications'
+                    url: `${process.env.URL_SUCCESS_NOTIFICATION}`,
                 }
             },
         };
