9d8843a9fc572625a7b8b7e10129cc3392bb59a4
dezmerek <mszymanski027@gmail.com>
2023-12-15 01:07:21 +0100

    Introducing corrections to the display of the repertoire on the home page

--- frontend/src/components/home/Repertoire/Repertoire.jsx
+++ frontend/src/components/home/Repertoire/Repertoire.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import '../../../Styles/layout/_Repertoire.scss';
-import { format, addDays, startOfWeek } from 'date-fns';
+import { format, addDays } from 'date-fns';
 import { Link } from 'react-router-dom';
 import { pl } from 'date-fns/locale';
 import { IoMdNotificationsOutline } from 'react-icons/io';
@@ -17,36 +17,8 @@ const Repertoire = () => {
     return `${year}-${month}-${day}`;
   };
 
-  useEffect(() => {
-    const fetchScheduleForToday = async () => {
-      try {
-        const response = await fetch(
-          `${process.env.REACT_APP_API_URL}/api/schedules?date=${formatDate(
-            selectedDate
-          )}`
-        );
-        const data = await response.json();
-
-        const schedulesForSelectedDate = data.map((entry) => ({
-          movie: entry.movie.title,
-          language: entry.movie.language,
-          startTime: format(new Date(entry.startTime), 'HH:mm'),
-          isPremiere: entry.movie.isPremiere,
-        }));
-
-        setSelectedMovies(schedulesForSelectedDate);
-      } catch (error) {
-        console.error('Error fetching schedule:', error);
-      }
-    };
-
-    fetchScheduleForToday();
-  }, [selectedDate]);
-
-  const handleDateClick = async (dayIndex) => {
-    const clickedDate = new Date(
-      startOfWeek(selectedDate, { weekStartsOn: 1 })
-    );
+  const handleDateClick = useCallback(async (dayIndex) => {
+    const clickedDate = new Date();
     clickedDate.setDate(clickedDate.getDate() + dayIndex);
     clickedDate.setHours(0, 0, 0, 0);
 
@@ -72,36 +44,25 @@ const Repertoire = () => {
     } catch (error) {
       console.error('Error fetching schedule:', error);
     }
-  };
+  }, []);
+
+  useEffect(() => {
+    handleDateClick(0);
+  }, [handleDateClick]);
 
   const currentDate = new Date();
-  const startDate = startOfWeek(currentDate, { weekStartsOn: 1 });
 
   const renderDayButtons = () => {
     const buttons = [];
     let isEvenColor = false;
-    const daysOfWeek = [
-      'poniedziałek',
-      'wtorek',
-      'środa',
-      'czwartek',
-      'piątek',
-      'sobota',
-      'niedziela',
-    ];
-    const todayIndex = daysOfWeek.indexOf(
-      format(currentDate, 'iiii', { locale: pl })
-    );
 
     for (let i = 0; i < 7; i++) {
-      const dayIndex = (todayIndex + i) % 7; // Adjusted day index to start from today
-
-      const dayDate = addDays(startDate, dayIndex);
+      const dayDate = addDays(currentDate, i);
 
       buttons.push(
         <button
           key={i}
-          onClick={() => handleDateClick(dayIndex)}
+          onClick={() => handleDateClick(i)}
           className={`${
             format(selectedDate, 'yyyy-MM-dd') === format(dayDate, 'yyyy-MM-dd')
               ? 'active'
@@ -138,59 +99,61 @@ const Repertoire = () => {
         <div className="day-buttons">{renderDayButtons()}</div>
         <div>
           {selectedMovies.length > 0 ? (
-            <table>
-              <tbody>
-                {Object.entries(
-                  selectedMovies.reduce((acc, curr) => {
-                    if (!acc[curr.movie]) {
-                      acc[curr.movie] = {
-                        language: curr.language,
-                        times: [curr.startTime],
-                        isPremiere: curr.isPremiere,
-                      };
-                    } else {
-                      acc[curr.movie].times.push(curr.startTime);
-                    }
-                    return acc;
-                  }, {})
-                ).map(([movie, details], index) => (
-                  <tr key={index}>
-                    <td className="repertoire-view__movie-title">
-                      <span>
+            <div>
+              <table>
+                <tbody>
+                  {Object.entries(
+                    selectedMovies.reduce((acc, curr) => {
+                      if (!acc[curr.movie]) {
+                        acc[curr.movie] = {
+                          language: curr.language,
+                          times: [curr.startTime],
+                          isPremiere: curr.isPremiere,
+                        };
+                      } else {
+                        acc[curr.movie].times.push(curr.startTime);
+                      }
+                      return acc;
+                    }, {})
+                  ).map(([movie, details], index) => (
+                    <tr key={index}>
+                      <td className="repertoire-view__movie-title">
+                        <span>
+                          {movie}
+                          {details.isPremiere && <IoMdNotificationsOutline />}
+                        </span>
+                        <span>{details.language}</span>
+                      </td>
+
+                      <td className="repertoire-view__title">
                         {movie}
                         {details.isPremiere && <IoMdNotificationsOutline />}
-                      </span>
-                      <span>{details.language}</span>
-                    </td>
+                      </td>
 
-                    <td className="repertoire-view__title">
-                      {movie}
-                      {details.isPremiere && <IoMdNotificationsOutline />}
-                    </td>
-
-                    <td className="repertoire-view__language">
-                      {details.language}
-                    </td>
-                    <td className="repertoire-view__hours">
-                      {details.times.map((time, timeIndex) => (
-                        <Link
-                          key={timeIndex}
-                          to={`/`}
-                          className="time-slot-link"
-                        >
-                          <span key={timeIndex}>
-                            {time} {timeIndex !== details.times.length - 1}
-                          </span>
-                        </Link>
-                      ))}
-                    </td>
-                  </tr>
-                ))}
-                <p className="repertoire-view__legend">
-                  <IoMdNotificationsOutline /> - Premiera filmu
-                </p>
-              </tbody>
-            </table>
+                      <td className="repertoire-view__language">
+                        {details.language}
+                      </td>
+                      <td className="repertoire-view__hours">
+                        {details.times.map((time, timeIndex) => (
+                          <Link
+                            key={timeIndex}
+                            to={`/`}
+                            className="time-slot-link"
+                          >
+                            <span key={timeIndex}>
+                              {time} {timeIndex !== details.times.length - 1}
+                            </span>
+                          </Link>
+                        ))}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <p className="repertoire-view__legend">
+                <IoMdNotificationsOutline /> - Premiera filmu
+              </p>
+            </div>
           ) : (
             <p>Brak filmów na wybrany dzień.</p>
           )}
