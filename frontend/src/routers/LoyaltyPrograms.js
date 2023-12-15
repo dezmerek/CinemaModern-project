9143eed78bab000d33451b76b825a795bd2571a0
dezmerek <mszymanski027@gmail.com>
2023-12-15 01:42:22 +0100

    Setting the page title

--- frontend/src/routers/LoyaltyPrograms.js
+++ frontend/src/routers/LoyaltyPrograms.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../components/home/Navbar/Navbar";
 import Footer from "../components/home/Footer/Footer";
 import Contact from "../components/home/Contact/Contact";
 import LoyaltyProgram from "../components/home/loyaltyProgram/loyaltyProgram";
 
 const LoyaltyPrograms = () => {
+    useEffect(() => {
+        document.title = 'CinemaCity - Program lojalnościowy';
+    }, []);
     return (
         <div>
             <Navbar />
